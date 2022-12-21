a3de6a294dad820c171fb83e6d1246e57e70b183
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-21 20:32:57 +0200

    format all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 import Section from './Section/Section';
 import Contacts from './Contacts/Contacts';
@@ -6,8 +6,10 @@ import FormAddContact from './FormAddContact/FormAddContact';
 import FilterContact from './FilterContact/FilterContact';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contactsList')) || [])
-  const [filter, setFilter] = useState("")
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contactsList')) || []
+  );
+  const [filter, setFilter] = useState('');
 
   const addContact = contact => {
     if (contacts.find(elem => elem.name === contact.name)) {
@@ -15,7 +17,7 @@ export const App = () => {
       return;
     }
     setContacts(prevContacts => [...prevContacts, contact]);
-};
+  };
 
   const removeContact = event => {
     const idContactToRemove = event.target.attributes.id.nodeValue;
@@ -23,42 +25,38 @@ export const App = () => {
       elem => elem.id !== idContactToRemove
     );
     setContacts(arrayContacts);
-};
+  };
 
- const findContactsByName = event =>
+  const findContactsByName = event =>
     setFilter(event.target.value.trim().toLowerCase());
- 
+
   useEffect(() => {
-    localStorage.setItem("contactsList", JSON.stringify(contacts));
+    localStorage.setItem('contactsList', JSON.stringify(contacts));
   }, [contacts]); // render-1 -> uE | render-2 -> uE | render-3 -> uE
 
-    const contactToFind = contacts.filter(elem =>
-      elem.name.toLowerCase().includes(filter)
-    );
-    return (
-      <div
-        style={{
-          height: '100%',
-          display: 'flex',
-          flexDirection: `column`,
-          alignItems: `center`,
-          color: '#010101',
-        }}
-      >
-        <Section title={'Phonebook'}>
-          <FormAddContact addContactOnSubmit={addContact} />
-        </Section>
-        <Section title={'Contacts'}>
-          <FilterContact
-            findContactsByName={findContactsByName}
-            filters={filter}
-          />
-          <Contacts
-            contacts={contactToFind}
-            removeContacts={removeContact}
-          />
-        </Section>
-      </div>
-    );
-  }
-
+  const contactToFind = contacts.filter(elem =>
+    elem.name.toLowerCase().includes(filter)
+  );
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: `column`,
+        alignItems: `center`,
+        color: '#010101',
+      }}
+    >
+      <Section title={'Phonebook'}>
+        <FormAddContact addContactOnSubmit={addContact} />
+      </Section>
+      <Section title={'Contacts'}>
+        <FilterContact
+          findContactsByName={findContactsByName}
+          filters={filter}
+        />
+        <Contacts contacts={contactToFind} removeContacts={removeContact} />
+      </Section>
+    </div>
+  );
+};
